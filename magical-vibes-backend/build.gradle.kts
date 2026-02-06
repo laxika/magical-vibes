88c43bbcb77cab8073a9107cd301cd458dbbdf19
Gyula Lakatos <laxika91@gmail.com>
2026-02-06 21:34:23 +0100

    Started working on the backend.

--- magical-vibes-backend/build.gradle.kts
+++ magical-vibes-backend/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     java
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 buildscript {
     repositories {
         mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:4.0.1")
     }
 }
 
@@ -12,6 +16,17 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 }
 
 group = "com.magicalvibes"
---
+++ magical-vibes-backend/src/main/java/com/github/laxika/magicalvibes/MagicalVibesApplication.java
@@ -0,0 +1,9 @@
+package com.github.laxika.magicalvibes;
+
+@SpringBootApplication
+public class MagicalVibesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicalVibesApplication.class, args);
+    }
+}
