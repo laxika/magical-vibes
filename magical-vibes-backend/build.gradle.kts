28176acaedebf76f21ba33badbe14acc52889217
Gyula Lakatos <laxika91@gmail.com>
2026-02-06 22:47:38 +0100

    Added the first iteration of the game.

--- magical-vibes-backend/build.gradle.kts
+++ magical-vibes-backend/build.gradle.kts
@@ -22,8 +22,10 @@ dependencyManagement {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-jackson")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.liquibase:liquibase-core")
     runtimeOnly("org.xerial:sqlite-jdbc:3.47.1.0")
     runtimeOnly("org.hibernate.orm:hibernate-community-dialects:6.6.4.Final")
 }
