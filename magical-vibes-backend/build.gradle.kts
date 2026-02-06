93c0e7470cada7d5abcb6c5a6b193594107481a1
Gyula Lakatos <laxika91@gmail.com>
2026-02-06 23:05:47 +0100

    Fix Liquibase issues.

--- magical-vibes-backend/build.gradle.kts
+++ magical-vibes-backend/build.gradle.kts
@@ -24,10 +24,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-jackson")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.liquibase:liquibase-core")
+    implementation("org.springframework.boot:spring-boot-starter-liquibase")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     runtimeOnly("org.xerial:sqlite-jdbc:3.47.1.0")
     runtimeOnly("org.hibernate.orm:hibernate-community-dialects:6.6.4.Final")
 }
