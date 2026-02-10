afbb746bf6eb46e1d91063f74cbb6aab5e4c92b4
Gyula Lakatos <laxika91@gmail.com>
2026-02-10 23:27:54 +0100

    Refactor.

--- magical-vibes-backend/src/main/java/com/github/laxika/magicalvibes/config/JacksonConfig.java
+++ magical-vibes-backend/src/main/java/com/github/laxika/magicalvibes/config/JacksonConfig.java
@@ -1,17 +1,8 @@
 package com.github.laxika.magicalvibes.config;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.github.laxika.magicalvibes.model.CardSubtype;
 import com.github.laxika.magicalvibes.model.CardType;
-import com.github.laxika.magicalvibes.model.effect.AwardManaEffect;
-import com.github.laxika.magicalvibes.model.effect.CardEffect;
-import com.github.laxika.magicalvibes.model.effect.GainLifeEffect;
-import com.github.laxika.magicalvibes.model.effect.GainLifeEqualToToughnessEffect;
-import com.github.laxika.magicalvibes.model.effect.GainLifePerGraveyardCardEffect;
-import com.github.laxika.magicalvibes.model.effect.GrantKeywordToTargetEffect;
-import com.github.laxika.magicalvibes.model.effect.OpponentMayPlayCreatureEffect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.ObjectMapper;
@@ -25,7 +16,6 @@ public class JacksonConfig {
         return JsonMapper.builder()
                 .addMixIn(CardType.class, CardTypeMixin.class)
                 .addMixIn(CardSubtype.class, CardSubtypeMixin.class)
-                .addMixIn(CardEffect.class, CardEffectMixin.class)
                 .build();
     }
 
@@ -37,17 +27,5 @@ public class JacksonConfig {
     static abstract class CardSubtypeMixin {
         @JsonValue
         abstract String getDisplayName();
-    }
-
-    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "effectType")
-    @JsonSubTypes({
-            @JsonSubTypes.Type(value = AwardManaEffect.class, name = "AWARD_MANA"),
-            @JsonSubTypes.Type(value = GainLifeEffect.class, name = "GAIN_LIFE"),
-            @JsonSubTypes.Type(value = GainLifePerGraveyardCardEffect.class, name = "GAIN_LIFE_PER_GRAVEYARD_CARD"),
-            @JsonSubTypes.Type(value = GrantKeywordToTargetEffect.class, name = "GRANT_KEYWORD_TO_TARGET"),
-            @JsonSubTypes.Type(value = OpponentMayPlayCreatureEffect.class, name = "OPPONENT_MAY_PLAY_CREATURE"),
-            @JsonSubTypes.Type(value = GainLifeEqualToToughnessEffect.class, name = "GAIN_LIFE_EQUAL_TO_TOUGHNESS")
-    })
-    static abstract class CardEffectMixin {
     }
 }
