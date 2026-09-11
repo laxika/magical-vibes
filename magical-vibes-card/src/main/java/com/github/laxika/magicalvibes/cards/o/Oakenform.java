a9bcd9d7aeb89702edc9b13a335ee82dc2084f5b
Laxika <laxika91@gmail.com>
2026-09-11 18:48:38 +0200

    Register W16 set and 16 card printings

--- magical-vibes-card/src/main/java/com/github/laxika/magicalvibes/cards/o/Oakenform.java
+++ magical-vibes-card/src/main/java/com/github/laxika/magicalvibes/cards/o/Oakenform.java
@@ -10,6 +10,7 @@ import com.github.laxika.magicalvibes.model.filter.TargetFilters;
 @CardRegistration(set = "M10", collectorNumber = "197")
 @CardRegistration(set = "M19", collectorNumber = "191")
 @CardRegistration(set = "M20", collectorNumber = "341")
+@CardRegistration(set = "W16", collectorNumber = "15")
 public class Oakenform extends Card {
 
     public Oakenform() {
